e815927178be9eec3bd84df1850b6ddf67e8a31f
leader with sadiq <gm1sadiq@gmail.com>
2021-10-30 11:18:06 +0600

    tour-server-site

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yoxzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run (){
+    try{
+        await client.connect();
+        const database = client.db('amazing_tour')
+        const serviceCollection = database.collection('services');
+        
+        //GET Services
+        app.get('/services', async(req,res) =>{
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        //GET single Service
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            console.log('right id',id);
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.json(service);
+        })
+
+        // POST API
+        app.post('/services', async(req, res)=>{
+            const service = req.body;
+            console.log('hit the api', service);
+            const result = await serviceCollection.insertOne(service)
+            res.json(result)
+        });
+        
+        //DELETE API
+        app.delete('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await serviceCollection.deleteOne(query);
+            res.json(result)
+        })
+
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Ema jon server is running');
+});
+
+app.listen(port, () => {
+    console.log('Server running at port', port);
+})
